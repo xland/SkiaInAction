5e8754d65088ba1ad8c7f979261e684383f212e5
unknown <412588801@qq.com>
2024-01-11 16:38:22 +0800

    main

--- RasterAndOpenGL/src/main.cpp
+++ RasterAndOpenGL/src/main.cpp
@@ -1,35 +1,43 @@
-#include <Windows.h>
+#include <windows.h>
+#include <windowsx.h>
+#include <string>
+#include <format>
 #include "include/core/SkGraphics.h"
-#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+
+void initWindow() {
+    std::wstring clsName{ L"SkiaInAction" };
+    auto hinstance = GetModuleHandle(NULL);
+    WNDCLASSEX wcx{};
+    wcx.cbSize = sizeof(WNDCLASSEX);
+    wcx.style = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS | CS_OWNDC;
+    //wcx.lpfnWndProc = WndProc;
+    wcx.cbClsExtra = 0;
+    wcx.cbWndExtra = 0;
+    wcx.hInstance = hinstance;
+    wcx.hIcon = LoadIcon(hinstance, (LPCTSTR)IDI_WINLOGO);
+    wcx.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcx.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcx.lpszMenuName = nullptr;
+    wcx.lpszClassName = clsName.c_str();
+    wcx.hIconSm = LoadIcon(hinstance, (LPCTSTR)IDI_WINLOGO);
+    if (!RegisterClassEx(&wcx))
+    {
+        return;
+    }
+    auto hwnd = CreateWindow(clsName.c_str(), nullptr, WS_OVERLAPPEDWINDOW,100, 100, 800, 600,
+        nullptr, nullptr, hinstance, nullptr);;
+}
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPTSTR lpCmdLine, _In_ int nCmdShow)
 {
-    HANDLE mutex = CreateMutex(NULL, TRUE, L"ScreenCapture");
-    if (mutex == NULL)
-    {
-        MessageBox(NULL, L"Failed to create process mutex.", L"Error", MB_OK | MB_ICONERROR);
-        return -1;
-    }
-    if (GetLastError() == ERROR_ALREADY_EXISTS)
-    {
-        CloseHandle(mutex);
-        return -1;
-    }
-    HRESULT hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE);
-    if (FAILED(hr))
-    {
-        MessageBox(NULL, L"Failed to initialize COM library.", L"Error", MB_OK | MB_ICONERROR);
-        return -1;
-    }
     SkGraphics::Init();
+    initWindow();
     MSG msg;
     while (GetMessage(&msg, NULL, 0, 0))
     {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
-    CoUninitialize();
-    CloseHandle(mutex);
-    auto code = App::GetExitCode();
-    return code;
+    return 0;
 }
