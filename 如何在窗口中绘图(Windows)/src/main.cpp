04243293e30fa1313d2c5dee35e40da6487348dc
xland <412588801@qq.com>
2024-07-03 15:03:07 +0800

    Update main.cpp

--- 如何在窗口中绘图(Windows)/src/main.cpp
+++ 如何在窗口中绘图(Windows)/src/main.cpp
@@ -1,25 +1,18 @@
 #include <windows.h>
-#include <windowsx.h>
 #include <string>
-#include <format>
-#include <memory>
 
-#include "include/core/SkGraphics.h"
 #include "include/core/SkSurface.h"
 #include "include/core/SkCanvas.h"
 #include "src/base/SkAutoMalloc.h"
 
-HWND hwnd;
-sk_sp<SkSurface> surface;
+
 int w{ 800 }, h{ 600 };
 SkAutoMalloc surfaceMemory;
 
 void initSurface() {
-    surface.reset();
     surfaceMemory.reset(h * 4 * w);
     SkImageInfo info = SkImageInfo::MakeN32Premul(w, h);
-    surface = SkSurfaces::WrapPixels(info, surfaceMemory.get(), 4 * w);
-
+    sk_sp<SkSurface> surface = SkSurfaces::WrapPixels(info, surfaceMemory.get(), 4 * w);
     SkCanvas* canvas = surface->getCanvas();
     canvas->clear(SK_ColorBLACK);
     SkPaint paint;
@@ -28,30 +21,33 @@ void initSurface() {
 }
 
 LRESULT CALLBACK wndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    PAINTSTRUCT ps;
+{    
     switch (message) {
-    case WM_CLOSE: {
-        PostQuitMessage(0);
-        return true;
-    }
-    case WM_SIZE: {
-        w = LOWORD(lParam);
-        h = HIWORD(lParam);
-        initSurface();
-        return true;
-    }
-    case WM_PAINT: {
-        auto dc = BeginPaint(hWnd, &ps); 
-        BITMAPINFO info = { sizeof(BITMAPINFOHEADER), w, 0 - h, 1, 32, BI_RGB, h * 4 * w, 0, 0, 0, 0 };
-        StretchDIBits(dc, 0, 0, w, h, 0, 0, w, h, surfaceMemory.get(), &info, DIB_RGB_COLORS, SRCCOPY);
-        ReleaseDC(hWnd, dc);
-        EndPaint(hWnd, &ps);
-        surfaceMemory.reset(0);
-        return true;
-    }
-    default:
-        break;
+        case WM_CLOSE: {
+            PostQuitMessage(0);
+            break;
+        }
+        case WM_SIZE: {
+            w = LOWORD(lParam);
+            h = HIWORD(lParam);
+            if (w > 0 && h > 0) {
+                initSurface();
+            }
+            break;
+        }
+        case WM_PAINT: {
+            PAINTSTRUCT ps;
+            auto dc = BeginPaint(hWnd, &ps); 
+            BITMAPINFO info = { sizeof(BITMAPINFOHEADER), w, 0 - h, 1, 32, BI_RGB, h * 4 * w, 0, 0, 0, 0 };
+            StretchDIBits(dc, 0, 0, w, h, 0, 0, w, h, surfaceMemory.get(), &info, DIB_RGB_COLORS, SRCCOPY);
+            ReleaseDC(hWnd, dc);
+            EndPaint(hWnd, &ps);
+            surfaceMemory.reset(0);
+            break;
+        }
+        default: {
+            break;
+        }
     }
     return DefWindowProc(hWnd, message, wParam, lParam);
 }
@@ -61,21 +57,14 @@ void initWindow() {
     auto hinstance = GetModuleHandle(NULL);
     WNDCLASSEX wcx{};
     wcx.cbSize = sizeof(WNDCLASSEX);
-    wcx.style = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS | CS_OWNDC;
+    wcx.style = CS_HREDRAW | CS_VREDRAW;
     wcx.lpfnWndProc = wndProc;
-    wcx.cbClsExtra = 0;
-    wcx.cbWndExtra = 0;
-    wcx.hInstance = hinstance;
-    wcx.hIcon = LoadIcon(hinstance, (LPCTSTR)IDI_WINLOGO);
     wcx.hCursor = LoadCursor(nullptr, IDC_ARROW);
-    wcx.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-    wcx.lpszMenuName = nullptr;
     wcx.lpszClassName = clsName.c_str();
-    wcx.hIconSm = LoadIcon(hinstance, (LPCTSTR)IDI_WINLOGO);
     if (!RegisterClassEx(&wcx)) {
         return;
     }
-    hwnd = CreateWindow(clsName.c_str(), clsName.c_str(), WS_OVERLAPPEDWINDOW, 100, 100, w, h, nullptr, nullptr, hinstance, nullptr);
+    auto hwnd = CreateWindow(clsName.c_str(), clsName.c_str(), WS_OVERLAPPEDWINDOW, 100, 100, w, h, nullptr, nullptr, hinstance, nullptr);
     ShowWindow(hwnd, SW_SHOW);
 }
 
