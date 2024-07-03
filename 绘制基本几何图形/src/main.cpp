3c1994de2decd7e0144523524aa8f57f4caa3ff9
xland <412588801@qq.com>
2024-07-03 12:46:05 +0800

    Update main.cpp

--- 绘制基本几何图形/src/main.cpp
+++ 绘制基本几何图形/src/main.cpp
@@ -2,21 +2,31 @@
 #include "include/core/SkSurface.h"
 #include "include/core/SkCanvas.h"
 #include "include/encode/SkPngEncoder.h"
+#include "include/core/SkBitmap.h"
 #include "include/core/SkStream.h"
+#include "include/codec/SkCodec.h"
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPTSTR lpCmdLine, _In_ int nCmdShow)
 {
+    //auto inputStream = std::make_unique<SkFILEStream>("allen-image1.png");
+    //auto codec = SkCodec::MakeFromStream(std::move(inputStream));
+    //auto imgInfo = codec->getInfo();
+    //SkBitmap bitmap;
+    //bitmap.allocPixels(imgInfo);
+    //SkCodec::Result result = codec->getPixels(imgInfo, bitmap.getPixels(), bitmap.rowBytes());
+    //auto surface = SkSurfaces::WrapPixels(bitmap.pixmap());
+
+
     auto imgInfo = SkImageInfo::MakeN32Premul(800, 600);
     auto surface = SkSurfaces::Raster(imgInfo);
-    
     auto canvas = surface->getCanvas();
     SkPaint paint;
-    paint.setStroke(true);
-    paint.setStrokeWidth(3);
-    paint.setColor(SK_ColorGREEN);
+    //paint.setStroke(true);
+    //paint.setStrokeWidth(3);
+    paint.setColor(SK_ColorGREEN);//SK_ColorGREEN
     SkRect rect;
-    //rect.setXYWH(10, 10, 100, 100);
-    rect.setLTRB(10, 10, 110, 110);
+    rect.setXYWH(10, 10, 100, 100);
+    //rect.setLTRB(10, 10, 110, 110);
     canvas->drawRect(rect,paint);
     auto image = surface->makeImageSnapshot();
     SkPixmap pixmap;
