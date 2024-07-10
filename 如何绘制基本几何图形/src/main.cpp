bdf742f0f3cfb6c332a86e2e4d475a3f6cde2d92
xland <412588801@qq.com>
2024-07-10 10:14:54 +0800

    1

--- 如何绘制基本几何图形/src/main.cpp
+++ 如何绘制基本几何图形/src/main.cpp
@@ -5,21 +5,22 @@
 #include "src/base/SkAutoMalloc.h"
 #include "include/core/SkRRect.h"
 
-
-int w{ 800 }, h{ 600 };
+int w{800}, h{600};
 SkAutoMalloc surfaceMemory;
 
-void drawCircle(SkCanvas* canvas) {
+void drawCircle(SkCanvas *canvas)
+{
     SkPaint paint;
     paint.setColor(SK_ColorRED);
     paint.setAntiAlias(true);
     auto x = w / 2;
     auto y = h / 2;
-    auto r = std::min(x-10, y-10);
+    auto r = std::min(x - 10, y - 10);
     canvas->drawCircle(x, y, r, paint);
 }
 
-void drawEllipse(SkCanvas* canvas) {
+void drawEllipse(SkCanvas *canvas)
+{
     SkPaint paint;
     paint.setColor(SK_ColorRED);
     paint.setAntiAlias(true);
@@ -28,24 +29,26 @@ void drawEllipse(SkCanvas* canvas) {
     canvas->drawOval(rect, paint);
 }
 
-void drawRRect(SkCanvas* canvas) {
+void drawRRect(SkCanvas *canvas)
+{
     SkPaint paint;
     paint.setColor(SK_ColorRED);
     paint.setAntiAlias(true);
     SkRect rect;
-    rect.setLTRB(60, 60, w -60, h - 60);
+    rect.setLTRB(60, 60, w - 60, h - 60);
     SkVector radii[4]{
-        {16, 16},  // 矩形左上角圆角尺寸；
-        {16, 16},  // 矩形右上角圆角尺寸；
-        {16, 16},  // 矩形右下角圆角尺寸；
-        {16, 16}   // 矩形左下角圆角尺寸；
+        {16, 16}, // 矩形左上角圆角尺寸；
+        {16, 16}, // 矩形右上角圆角尺寸；
+        {16, 16}, // 矩形右下角圆角尺寸；
+        {16, 16}  // 矩形左下角圆角尺寸；
     };
     SkRRect rr;
     rr.setRectRadii(rect, radii);
     canvas->drawRRect(rr, paint);
 }
 
-void drawLine(SkCanvas* canvas) {
+void drawLine(SkCanvas *canvas)
+{
     SkPaint paint;
     paint.setColor(SK_ColorRED);
     paint.setAntiAlias(true);
@@ -55,7 +58,8 @@ void drawLine(SkCanvas* canvas) {
     canvas->drawLine(80, 80, w - 80, h - 80, paint);
 }
 
-void drawArc(SkCanvas* canvas) {
+void drawArc(SkCanvas *canvas)
+{
     SkPaint paint;
     paint.setColor(SK_ColorRED);
     paint.setAntiAlias(true);
@@ -67,38 +71,75 @@ void drawArc(SkCanvas* canvas) {
     canvas->drawArc(rect, 0, -90, false, paint);
 }
 
-void drawPoint(SkCanvas* canvas) {
+void drawPoint(SkCanvas *canvas)
+{
     SkPaint paint;
     paint.setColor(SK_ColorRED);
     paint.setAntiAlias(true);
     paint.setStroke(true);
     paint.setStrokeWidth(16);
     paint.setStrokeCap(SkPaint::Cap::kRound_Cap);
-    //canvas->drawPoint(w/2,h/2, paint);
+    // canvas->drawPoint(w/2,h/2, paint);
 
-    SkPoint pts[]{ SkPoint::Make(60,60),
-        SkPoint::Make(w / 2,h / 2), 
-        SkPoint::Make(w -60,h -60) };
-    canvas->drawPoints(SkCanvas::PointMode::kPoints_PointMode,3,pts, paint);
+    SkPoint pts[]{SkPoint::Make(60, 60),
+                  SkPoint::Make(w / 2, h / 2),
+                  SkPoint::Make(w - 60, h - 60)};
+    canvas->drawPoints(SkCanvas::PointMode::kPoints_PointMode, 3, pts, paint);
 }
 
-void setPixel() {    
+void drawPath(SkCanvas *canvas)
+{
+    SkPaint paint;
+    paint.setAntiAlias(true);
+    paint.setColor(0xFF00FF66);
+    paint.setStroke(true);
+    paint.setStrokeWidth(16);
+    paint.setStrokeJoin(SkPaint::kRound_Join);
+    SkPath path;
+    path.moveTo(60, 120);
+    path.lineTo(180, 60);
+    path.lineTo(w - 60, 120);
+    path.lineTo(w - 160, h - 160);
+    path.lineTo(180, h - 60);
+    path.close();
+    canvas->drawPath(path, paint);
+}
+
+void drawBezierPath(SkCanvas *canvas)
+{
+    SkPaint paint;
+    paint.setAntiAlias(true);
+    paint.setColor(0xFF00FF66);
+    paint.setStroke(true);
+    paint.setStrokeCap(SkPaint::Cap::kRound_Cap);
+    paint.setStrokeWidth(6);
+    SkPath path;
+    path.moveTo(60, 60);
+    path.cubicTo(280, 60, w - 280, h - 60, w - 60, h - 60);
+    canvas->drawPath(path, paint);
+}
+
+void setPixel()
+{
     surfaceMemory.reset(h * 4 * w);
     SkImageInfo info = SkImageInfo::MakeN32Premul(w, h);
     auto canvas = SkCanvas::MakeRasterDirect(info, surfaceMemory.get(), 4 * w);
     canvas->clear(SK_ColorBLACK);
-    //drawCircle(canvas.get());
-    //drawEllipse(canvas.get());
-    //drawRRect(canvas.get());
-    //drawLine(canvas.get());
-    //drawArc(canvas.get());
-    drawPoint(canvas.get());
+    drawCircle(canvas.get());
+    // drawEllipse(canvas.get());
+    // drawRRect(canvas.get());
+    // drawLine(canvas.get());
+    // drawArc(canvas.get());
+    // drawPoint(canvas.get());
+    // drawPath(canvas.get());
+    // drawBezierPath(canvas.get());
 }
 
-void paint(const HWND hWnd) {
+void paint(const HWND hWnd)
+{
     PAINTSTRUCT ps;
     auto dc = BeginPaint(hWnd, &ps);
-    BITMAPINFO info = { sizeof(BITMAPINFOHEADER), w, 0 - h, 1, 32, BI_RGB, h * 4 * w, 0, 0, 0, 0 };
+    BITMAPINFO info = {sizeof(BITMAPINFOHEADER), w, 0 - h, 1, 32, BI_RGB, h * 4 * w, 0, 0, 0, 0};
     StretchDIBits(dc, 0, 0, w, h, 0, 0, w, h, surfaceMemory.get(), &info, DIB_RGB_COLORS, SRCCOPY);
     ReleaseDC(hWnd, dc);
     EndPaint(hWnd, &ps);
@@ -106,33 +147,40 @@ void paint(const HWND hWnd) {
 }
 
 LRESULT CALLBACK wndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{    
-    switch (message) {
-        case WM_SIZE: {
-            w = LOWORD(lParam);
-            h = HIWORD(lParam);
-            if (wParam != SIZE_MINIMIZED) {
-                setPixel();
-            }            
-            break;
+{
+    switch (message)
+    {
+    case WM_SIZE:
+    {
+        w = LOWORD(lParam);
+        h = HIWORD(lParam);
+        if (wParam != SIZE_MINIMIZED)
+        {
+            setPixel();
         }
-        case WM_PAINT: {
-            paint(hWnd);
-            break;
-        }
-        case WM_CLOSE: {
-            PostQuitMessage(0);
-            break;
-        }
-        default: {
-            break;
-        }
+        break;
+    }
+    case WM_PAINT:
+    {
+        paint(hWnd);
+        break;
+    }
+    case WM_CLOSE:
+    {
+        PostQuitMessage(0);
+        break;
+    }
+    default:
+    {
+        break;
+    }
     }
     return DefWindowProc(hWnd, message, wParam, lParam);
 }
 
-void initWindow() {
-    std::wstring clsName{ L"DrawInWindow" };
+void initWindow()
+{
+    std::wstring clsName{L"DrawInWindow"};
     auto hinstance = GetModuleHandle(NULL);
     WNDCLASSEX wcx{};
     wcx.cbSize = sizeof(WNDCLASSEX);
@@ -140,12 +188,13 @@ void initWindow() {
     wcx.lpfnWndProc = wndProc;
     wcx.hCursor = LoadCursor(nullptr, IDC_ARROW);
     wcx.lpszClassName = clsName.c_str();
-    if (!RegisterClassEx(&wcx)) {
+    if (!RegisterClassEx(&wcx))
+    {
         return;
     }
-    auto hwnd = CreateWindow(clsName.c_str(), clsName.c_str(), WS_OVERLAPPEDWINDOW, 
-        CW_USEDEFAULT, CW_USEDEFAULT, w, h, 
-        nullptr, nullptr, hinstance, nullptr);
+    auto hwnd = CreateWindow(clsName.c_str(), clsName.c_str(), WS_OVERLAPPEDWINDOW,
+                             CW_USEDEFAULT, CW_USEDEFAULT, w, h,
+                             nullptr, nullptr, hinstance, nullptr);
     ShowWindow(hwnd, SW_SHOW);
 }
 
@@ -153,7 +202,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
 {
     initWindow();
     MSG msg;
-    while (GetMessage(&msg, NULL, 0, 0)) {
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
