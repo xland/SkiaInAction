7d84e77e26587235a0c9ed8066e114db22d9ba76
xland <412588801@qq.com>
2024-07-15 11:16:59 +0800

    Update main.cpp

--- 表面/src/main.cpp
+++ 表面/src/main.cpp
@@ -21,115 +21,10 @@
 int w{400}, h{400};
 SkAutoMalloc surfaceMemory;
 
-void translateCanvas(SkCanvas* canvas) {
-    auto rect = SkRect::MakeXYWH(10, 10, 80, 80);
-    SkPaint paint;
-    paint.setColor(0xff00ffff);
-    canvas->drawRect(rect, paint);
-    canvas->translate(90, 90);
-    paint.setColor(0xffff00ff);
-    canvas->drawRect(rect, paint);
-    canvas->translate(90, 90);
-    paint.setColor(0xffffff00);
-    canvas->drawRect(rect, paint);
-}
-
-void rotateCanvas(SkCanvas* canvas) {
-    auto rect = SkRect::MakeXYWH(w/2-50, h/2-100, 100, 200);
-    SkPaint paint;
-    paint.setColor(0xff00ffff);
-    canvas->drawRect(rect, paint);
-    canvas->rotate(-45, w / 2, h / 2);
-    paint.setColor(0xffffff00);
-    canvas->drawRect(rect, paint);
-}
-
-void skewCanvas(SkCanvas* canvas) {
-    auto rect = SkRect::MakeXYWH(60, 60, 80, 80);
-    SkPaint paint;
-    paint.setAntiAlias(true);
-    paint.setColor(0xff00ffff);
-    canvas->drawRect(rect, paint);
-
-    canvas->save();
-    canvas->translate(60, 140);
-    canvas->skew(1, 0);
-    paint.setColor(0xffff00ff);
-    paint.setStroke(false);
-    rect = SkRect::MakeXYWH(0, 0, 80, 80);
-    canvas->drawRect(rect, paint);
-
-    canvas->restore();
-    canvas->translate(140, 60);
-    canvas->skew(0, 1);
-    paint.setColor(0xffffff00);
-    paint.setStroke(false);
-    rect = SkRect::MakeXYWH(0, 0, 80, 80);
-    canvas->drawRect(rect, paint);
-}
 
 void saveCanvas(SkCanvas* canvas) {
-    auto rect = SkRect::MakeXYWH(20, 20, 100, 100);
-    SkPaint paint;
-    paint.setAntiAlias(true);
-    paint.setColor(0xff00ffff);
-    canvas->drawRect(rect, paint);
-
-    SkRect bounds = SkRect::MakeLTRB(0, 0, 160, 160);
-    SkPaint layerPaint;
-    layerPaint.setAlphaf(0.5f);
-    canvas->saveLayer(&bounds, &layerPaint);
-
-    rect = SkRect::MakeXYWH(80, 80, 200, 200);
-    paint.setColor(0xffff00ff);
-    canvas->drawRect(rect, paint);
-    canvas->restore();
-}
-
-void clipCanvas(SkCanvas* canvas) {
-    canvas->save();
-    auto rect = SkRect::MakeXYWH(0, 0, w/2, h/2);
-    canvas->clipRect(rect);
-    canvas->clipShader()
-
-    SkPaint paint;
-    paint.setAntiAlias(true);
-    paint.setColor(0xff00ffff);
-    auto r = std::min(w / 2, h / 2);
-    canvas->drawCircle(SkPoint::Make(w / 2, h / 2), r,paint);
-    canvas->restore();
-
-    canvas->save();
-    rect = SkRect::MakeXYWH(w / 2, h / 2, w / 2, h / 2);
-    canvas->clipRect(rect);
-    paint.setColor(0xffffff00);
-    canvas->drawCircle(SkPoint::Make(w / 2, h / 2), r, paint);
-    canvas->restore();
-}
-
-
-void drawPixel(SkCanvas* canvas) {
-    SkImageInfo info = SkImageInfo::MakeN32Premul(w, h);
-    std::vector<SkColor> pixels;    
-    for (int y = 0; y < h; ++y) {
-        for (int x = 0; x < w; ++x) {
-            auto val = x % 30;
-            if (val == 0) {
-                pixels.push_back(0xFFFF00FF);
-            }
-            else if(val == 10)
-            {
-                pixels.push_back(0xFFFFFF00);
-            }
-            else if(val == 20) {
-                pixels.push_back(0xFF00FFFF);
-            }
-            else {
-                pixels.push_back(0xFF0000FF);
-            }
-        }
-    }
-    canvas->writePixels(info, &pixels.front(), w*4, 0, 0);
+    auto imgInfo = SkImageInfo::MakeN32Premul(800, 600);
+    auto surface = SkSurfaces::Raster(imgInfo);
 }
 
 void setPixel()
@@ -138,13 +33,7 @@ void setPixel()
     SkImageInfo info = SkImageInfo::MakeN32Premul(w, h);
     auto canvas = SkCanvas::MakeRasterDirect(info, surfaceMemory.get(), 4 * w);
     canvas->clear(SK_ColorBLACK);
-    //drawPixel(canvas.get());
-    //translateCanvas(canvas.get());
-    //skewCanvas(canvas.get());
-    saveCanvas(canvas.get());    
-    //clipCanvas(canvas.get());
-    //rotateCanvas(canvas.get());
-    
+    saveCanvas(canvas.get());
 }
 
 void paint(const HWND hWnd)
